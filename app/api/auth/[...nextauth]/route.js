15bd7cde167cda5df8dccf54e962cb624c75d70e
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-10-13 23:21:08 +0600

    .env file used

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,12 +5,12 @@ import FacebookProvider from 'next-auth/providers/facebook'
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "122647687706-5o4u39jtdpa2rreoa9mo7o9k92rtlqnv.apps.googleusercontent.com",
-            clientSecret: "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"
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
         }),
         FacebookProvider({
-            clientId: "849007810293198",
-            clientSecret: "35e50832dc194ee7df398a9a154fffc3"
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET
         })
     ]
 });
