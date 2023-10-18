4b3b11af23f897ff35b94e36167073a130d46806
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-10-18 19:52:17 +0600

    github credentials added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 import FacebookProvider from 'next-auth/providers/facebook'
+import GithubProvider from 'next-auth/providers/github'
 
 const handler = NextAuth({
     providers: [
@@ -11,6 +12,10 @@ const handler = NextAuth({
         FacebookProvider({
             clientId: process.env.FACEBOOK_ID,
             clientSecret: process.env.FACEBOOK_SECRET
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
         })
     ]
 });
