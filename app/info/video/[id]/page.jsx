2cb718fbaa0f1ca442704359929ebca4ca442e85
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-10-25 19:41:43 +0600

    bg dark when sidebar opend added

--- app/info/video/[id]/page.jsx
+++ app/info/video/[id]/page.jsx
@@ -1,10 +1,12 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import "@/public/CSS/Video.css";
+import { MyContext } from "@/app/layout";
 
 export default function Page(id) {
   const [video, setVideo] = useState([]);
+  const { isSideBarOpen } = useContext(MyContext);
   async function getVideo() {
     const req = await fetch(
       `https://api.themoviedb.org/3/${id.searchParams.details}/${id.params.id}/videos?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
@@ -19,7 +21,12 @@ export default function Page(id) {
   }, []);
 
   return (
-    <div className="video-container">
+    <div
+      className={
+        (isSideBarOpen && "sidebaropen  overflow-hidden video-container") ||
+        " overflow-hidden video-container"
+      }
+    >
       {(video &&
         video.length > 0 &&
         video.map((video) => {
@@ -28,7 +35,7 @@ export default function Page(id) {
               <ReactPlayer
                 style={{ width: "100%", height: "100%" }}
                 key={video.key}
-                className="movie_video"
+                className="movie_video "
                 url={`https://www.youtube.com/watch?v=${video.key}`}
               />
             </>
